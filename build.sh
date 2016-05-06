0d69360b3143d9093fa546a24feb0fddd1cec8d7
Chriztian Steinmeier <chriztian@steinmeier.dk>
2016-05-06 21:45:54 +0200

    Build version 1.2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-VERSION="1.0"
+VERSION="1.2"
 
 cd "$TM_PROJECT_DIRECTORY"
 
@@ -10,6 +10,7 @@ fi
 cp src/*.css dist/vokseverk/
 # cp src/*.js dist/vokseverk/
 cp src/package.manifest dist/vokseverk/package.manifest
+cp src/package.xml dist/vokseverk/package.xml
 
 # Build the ZIP file 
 zip -j "dist/zip/Vokseverk.UmbracoBackofficeTweaks-$VERSION.zip" dist/vokseverk/* -x \*.DS_Store
